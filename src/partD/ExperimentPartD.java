b565f5cc2b93f973b0c2f8c0380af737417f80f1
Samee24 <sameescouser24@gmail.com>
2014-09-09 12:06:06 -0500

    Update and rename TestPartD.java to ExperimentPartD.java

---
+++ src/partD/ExperimentPartD.java
@@ -0,0 +1,48 @@
+package partD;
+
+public class ExperimentPartD
+{
+  public static void main(String[] args)
+    throws Exception
+  {
+
+    PartD.nameGame("Superman");
+    PartD.nameGame("Shirley");
+    PartD.nameGame("Linocln");
+    PartD.nameGame("Samee");
+    PartD.nameGame("Ameer");
+    PartD.nameGame("Kyaruzi");
+    PartD.nameGame("Camila");
+    
+    
+  }
+}
+/** Output
+Superman!
+Superman, Superman bo Buperman Bonana fanna fo Fuperman
+Fee fy mo Muperman, Superman!
+
+Shirley!
+Shirley, Shirley bo Birley Bonana fanna fo Firley
+Fee fy mo Mirley, Shirley!
+
+Linocln!
+Linocln, Linocln bo Binocln Bonana fanna fo Finocln
+Fee fy mo Minocln, Linocln!
+
+Samee!
+Samee, Samee bo Bamee Bonana fanna fo Famee
+Fee fy mo Mamee, Samee!
+
+Ameer!
+Ameer, Ameer bo Beer Bonana fanna fo Feer
+Fee fy mo Meer, Ameer!
+
+Kyaruzi!
+Kyaruzi, Kyaruzi bo Baruzi Bonana fanna fo Faruzi
+Fee fy mo Maruzi, Kyaruzi!
+
+Camila!
+Camila, Camila bo Bamila Bonana fanna fo Famila
+Fee fy mo Mamila, Camila!
+*/
